7dad053d0d85abc0f70b0fc2f70640a453375d0a
Steve Danner <steve.danner@knowledgelake.com>
2013-07-31 14:15:26 -0500

    Changes made by Steve Danner (steve.danner@knowledgelake.com).

--- KnowledgeLake Search Mobile/app/init.test.js
+++ KnowledgeLake Search Mobile/app/init.test.js
@@ -17,7 +17,10 @@ require(["config"], function (config) {
              testRootPath + "system_tests",
              testRootPath + "homeViewModel_tests",
              testRootPath + "localization_tests",
-             testRootPath + "FileManagement_tests"
+             testRootPath + "FileManagement_tests",
+             testRootPath + "siteDataService_tests",
+             testRootPath + "websService_tests",
+             testRootPath + "authenticationService_tests"
              //ADDITIONAL TESTS GO HERE
     ],
     function($, ko, kendo, system, logLevel) {
