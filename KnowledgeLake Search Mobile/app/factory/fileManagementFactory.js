dc6debb03ddf79a9686f32a3582c12e5ba9ea613
Steve Danner <steve.danner@knowledgelake.com>
2013-08-20 09:53:49 -0500

    Changes made by Steve Danner (steve.danner@knowledgelake.com).

--- KnowledgeLake Search Mobile/app/factory/fileManagementFactory.js
+++ KnowledgeLake Search Mobile/app/factory/fileManagementFactory.js
@@ -1,19 +1,14 @@
 define(["framework/FileManagement", 
 		"mocks/localStorageFileManagement",
-		"mocks/fileManagementMock",
 		"system"],
-	function (File, localStorageFile, FileMock, system) {
+	function (File, localStorageFile, system) {
 		
 		//on device means use Cordova APIs for device storage
 		if (!system.isRunningInSimulator()) {
 			return new File();			
         }
 		//HTML5 localStorage available, use that
-		else if ('localStorage' in window && window['localStorage'] !== null) {
-			return new localStorageFile();
-        }
-		//if all else fails, use the in-memory file management
 		else {
-			return new FileMock();	
+			return new localStorageFile();
         }
     });
