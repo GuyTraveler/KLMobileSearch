f85559a02b1911dc2a8131ef8f763cf2b31583c2
Steve Danner <steve.danner@knowledgelake.com>
2013-09-10 14:37:56 -0500

    Changes made by Steve Danner (steve.danner@knowledgelake.com).

--- KnowledgeLake Search Mobile/app/unitTests/resultsViewModel_tests.js
+++ KnowledgeLake Search Mobile/app/unitTests/resultsViewModel_tests.js
@@ -43,7 +43,7 @@ define(['require',
 			//assert
             QUnit.equal(vm.resultDataSource(), resultData);
         });
-        
+       
         QUnit.test("test resultViewModel ctor", function () {
             //arrange
             var vm;
@@ -97,24 +97,36 @@ define(['require',
             //assert
             QUnit.ok(vm);
         });
-        
-        QUnit.test("test resultsViewModel show", function () {
+       
+        QUnit.asyncTest("test resultsViewModel show", function () {
             //arrange
             var vm,
-                savedSearchVM;
+                savedSearchVM,
+				keywordSearchPromise;
             
             savedSearchVM = new savedSearchViewModel();
 			vm = new resultsViewModel();
-            savedSearchVM.site = ko.observable(new site("http://prodsp2010.dev.local/sites/team4", "ProdSP2010", 15, new credential(credentialType.ntlm, "ryan.braun", "password", "dev"), "ryan"));
+            savedSearchVM.site = ko.observable(new site("http://prodsp2010.dev.local/sites/team4", "ProdSP2010", 15, new credential(credentialType.ntlm, "ryan.braun", "password", "dev")));
             savedSearchVM.keyword = ko.observable("ryan");
             
             window.savedSearchViewModel = savedSearchVM;
             
             //act
-            vm.show();
+            keywordSearchPromise = vm.show();
                         
             //assert
             QUnit.ok(vm);
+			QUnit.ok(keywordSearchPromise);
+			
+			keywordSearchPromise.done(function () {
+				QUnit.ok(true);
+				QUnit.start();
+            });
+			
+			keywordSearchPromise.fail(function () {
+				QUnit.ok(false);
+				QUnit.start();
+            });
         });
         
         QUnit.test("test resultsViewModel hide", function () {
@@ -254,7 +266,7 @@ define(['require',
                 QUnit.start();
             });
         });
-        
+      
         QUnit.asyncTest("test resultsViewModel keywordSearch bad credentials", function () {
             //arrange
             var vm,            
@@ -275,7 +287,7 @@ define(['require',
                 QUnit.equal(error.response, logonResponse.LogonFailed);
                 QUnit.start();
             });
-        });   
+        });
         
         QUnit.asyncTest("test resultsViewModel keywordSearch good credentials", function () {
             //arrange
