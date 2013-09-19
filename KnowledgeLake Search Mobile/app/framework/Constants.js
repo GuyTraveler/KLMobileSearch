405cb2f9806ef4c99a4b41cfe784e0a301c8f102
Ryan Braun <ryan.braun@knowledgelake.com>
2013-09-19 07:52:31 -0500

    Changes made by Ryan Braun (ryan.braun@knowledgelake.com).

--- KnowledgeLake Search Mobile/app/framework/Constants.js
+++ KnowledgeLake Search Mobile/app/framework/Constants.js
@@ -3,12 +3,6 @@ define({
     sharePoint2013MajorVersion: 15, 
     phrase: "Q2FyaGFydHQ=",
     
-    Contains: "Contains",
-    StartsWith: "Starts With",
-    Like: "Like",
-    IsNotNull: "Is Not Null",
-    Range: "Range",    
-    
     numberOperators: [ "=", ">", "<", ">=", "<=", system.strings.Range ],
     textboxOperators: [ "=", system.strings.Contains, system.strings.StartsWith, system.strings.Like, system.strings.IsNotNull ],
     dropdownOperators: [ "=", system.strings.Contains, system.strings.StartsWith, system.strings.IsNotNull ],
