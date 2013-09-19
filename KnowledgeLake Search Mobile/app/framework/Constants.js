8caa847d54b1f004d389a613dbe549dc6d93256b
Steve Danner <steve.danner@knowledgelake.com>
2013-09-19 08:02:24 -0500

    Changes made by Steve Danner (steve.danner@knowledgelake.com).

--- KnowledgeLake Search Mobile/app/framework/Constants.js
+++ KnowledgeLake Search Mobile/app/framework/Constants.js
@@ -1,14 +1,17 @@
-define({
-    sharePointDelimiter: ";#",
-    sharePoint2013MajorVersion: 15, 
-    phrase: "Q2FyaGFydHQ=",
-    
-    numberOperators: [ "=", ">", "<", ">=", "<=", system.strings.Range ],
-    textboxOperators: [ "=", system.strings.Contains, system.strings.StartsWith, system.strings.Like, system.strings.IsNotNull ],
-    dropdownOperators: [ "=", system.strings.Contains, system.strings.StartsWith, system.strings.IsNotNull ],
-    calendarOperators: [ "=", ">", "<", ">=", "<=", system.strings.Range, system.strings.IsNotNull ],
-    radiobuttonOperators: [ "=", system.strings.IsNotNull ],
-    comboboxOperators: [ "=", system.strings.Contains, system.strings.StartsWith, system.strings.IsNotNull ],
-    
-    radiobuttonValues: [ system.strings.Yes, system.strings.No, system.strings.NotSet ]
-});
+define(["system"], function (system) {
+
+		return {
+		    sharePointDelimiter: ";#",
+		    sharePoint2013MajorVersion: 15, 
+		    phrase: "Q2FyaGFydHQ=",
+		    
+		    numberOperators: [ "=", ">", "<", ">=", "<=", system.strings.Range ],
+		    textboxOperators: [ "=", system.strings.Contains, system.strings.StartsWith, system.strings.Like, system.strings.IsNotNull ],
+		    dropdownOperators: [ "=", system.strings.Contains, system.strings.StartsWith, system.strings.IsNotNull ],
+		    calendarOperators: [ "=", ">", "<", ">=", "<=", system.strings.Range, system.strings.IsNotNull ],
+		    radiobuttonOperators: [ "=", system.strings.IsNotNull ],
+		    comboboxOperators: [ "=", system.strings.Contains, system.strings.StartsWith, system.strings.IsNotNull ],
+		    
+		    radiobuttonValues: [ system.strings.Yes, system.strings.No, system.strings.NotSet ]
+		};
+	});
