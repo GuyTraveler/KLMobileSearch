c2a57d6e69b07665c3825bbd59b16841e6bfcd2b
Steve Danner <steve.danner@knowledgelake.com>
2013-10-11 14:59:54 -0500

    Changes made by Steve Danner (steve.danner@knowledgelake.com).

--- KnowledgeLake Search Mobile/app/services/soapQueryServiceBase.js
+++ KnowledgeLake Search Mobile/app/services/soapQueryServiceBase.js
@@ -2,15 +2,15 @@ define(["jquery",
         "knockout",
         "application",
 		"logger",
+		"domain/Constants",
         "domain/keywordConjunction",
         "ISearchService", 
         "services/soapParsingService"], 
-    function ($, ko, application, logger, keywordConjunction, searchService, SoapParsingService) {
+    function ($, ko, application, logger, Constants, keywordConjunction, searchService, SoapParsingService) {
         
         var soapQueryServiceBase = function (siteUrl, keywordTemplate, fnMassageKeyword) {
             var self = this,
-                buildKeywordClause,
-                maxResults = 100;  //TODO: change this to something higher once we have dynamic paging
+                buildKeywordClause;                
             
             self.keywordSearchAsync = function (keywordPhrases, conjunction, trimDuplicates) {
                 var clause = buildKeywordClause(keywordPhrases, conjunction),
@@ -20,7 +20,7 @@ define(["jquery",
                 
                 conjunction = conjunction ? conjunction : keywordConjunction.and;  //default to AND
                 trimDuplicates = (trimDuplicates && trimDuplicates !== false) ? true : false;
-                queryXml = keywordTemplate.replace("{clause}", clause).replace("{trimDuplicates}", trimDuplicates).replace("{maxResults}", maxResults);
+                queryXml = keywordTemplate.replace("{clause}", clause).replace("{trimDuplicates}", trimDuplicates).replace("{maxResults}", Constants.maxResults);
                 
 				logger.logVerbose("calling QueryEx with XML: " + queryXml);
                 
