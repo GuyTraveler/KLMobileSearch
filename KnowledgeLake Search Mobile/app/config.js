01c6919f612a86da566451a20177e4bb2110ca02
Steve Danner <steve.danner@knowledgelake.com>
2013-11-05 15:34:17 -0600

    Changes made by Steve Danner (steve.danner@knowledgelake.com).

--- KnowledgeLake Search Mobile/app/config.js
+++ KnowledgeLake Search Mobile/app/config.js
@@ -1,6 +1,6 @@
 define(["framework/logLevel"], function(logLevel) {
 	
-    var isUnitTesting = true, 
+    var isUnitTesting = false, 
 	    loggingLevel = logLevel.Verbose;
 	 
 	var config = {           
